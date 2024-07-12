79801a79d2f55f1eba306eed2d1ea5cad806108b
Saqib Ullah <ysaqibullah@gmail.com>
2024-07-12 11:16:40 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-const video = document.getElementById('video');
-const playPause = document.getElementById('play-pause');
-const stop = document.getElementById('stop');
-const seek = document.getElementById('seek');
-const imgchange = document.getElementById('changeImg');
-const largecont = document.getElementById('largeImage');
-function closeImage() {
+const vidchange = document.getElementById('changevideo');
+const largecont = document.getElementById('largevideo');
+function closeVideo() {
     largecont.style.display = 'none';
 }
-function ImageClick(source) {
-    imgchange.src = source;
+function videoClick(source) {
+    vidchange.src = source;
     largecont.style.display = 'block';
 }
-
-playPause.addEventListener('click', () => {
-  if (video.paused) {
-    video.play();
-    playPause.innerHTML = 'Pause';
-  } else {
-    video.pause();
-    playPause.innerHTML = 'Play';
-  }
-});
-
-stop.addEventListener('click', () => {
-  video.pause();
-  video.currentTime = 0;
-  playPause.innerHTML = 'Play';
-});
-
-video.addEventListener('timeupdate', () => {
-  const currentTime = video.currentTime;
-  const duration = video.duration;
-  const progress = (currentTime / duration) * 100;
-  seek.value = progress;
-});
 
